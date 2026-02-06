d8c0b9b7d8f5c8c6a5d242a625ad040d8d637bf8
lrs2187 <liurongshuo2022@outlook.com>
2026-02-06 20:58:44 +0800

    feat: 将 MainView 纳入服务主机

--- BBDown_GUI/App.axaml.cs
+++ BBDown_GUI/App.axaml.cs
@@ -30,11 +30,14 @@ public partial class App : Application
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
-            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow = new MainWindow
+            {
+                Content = IAppHost.GetService<MainView>()
+            };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
-            singleViewPlatform.MainView = new MainView();
+            singleViewPlatform.MainView = IAppHost.GetService<MainView>();
         }
 
         base.OnFrameworkInitializationCompleted();
@@ -62,6 +65,7 @@ public partial class App : Application
             {
                 services.AddSingleton<ConfigService>();
                 services.AddSingleton<ConfigHandler>();
+                services.AddSingleton<MainView>();
                 services.AddTransient<MainViewModel>();
                 
                 services.AddMainPage<HomePage>();
